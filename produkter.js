c4e202b81a7d6b560e96d24a0800cc8b6680f949
Philip Nordin <50575277+nordinphilipp@users.noreply.github.com>
2022-01-13 17:17:45 +0100

    Update produkter.js

--- produkter.js
+++ produkter.js
@@ -3,19 +3,19 @@ var produktArr =
   1: 
   {
     namn : "Iphone",
-	pris : 300,
+    pris : 300,
     img : "R.jpg"
   },
   2: 
   {
     namn : "Laptop",
-	pris : 100,
+    pris : 100,
     img : "q.JPG"
   },
   3: 
   {
     namn : "iPhone",
-	pris : 200,
+    pris : 200,
     img : "R.jpg"
   }
 };
